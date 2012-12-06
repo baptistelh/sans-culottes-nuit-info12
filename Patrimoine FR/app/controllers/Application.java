6dc7ab7a18e1c3f09eb688c48aa26a8177ec4094
flegaludec@gmail.com <flegaludec@gmail.com@61d47979-7446-dd67-8634-f3ecf0293d68>
2012-12-06 19:49:10 +0000

    ajout de la ville

--- Patrimoine FR/app/controllers/Application.java
+++ Patrimoine FR/app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import play.*;
+import play.cache.Cache;
+import play.db.jpa.Model;
 import play.mvc.*;
 
 import java.util.*;
@@ -8,13 +10,21 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
-
-    public static void index() {
-        render();
-    }
     
     public static void about() {
         render();
     }
+    
+ // lists all tweets ordered descendant & cache it
+ 	public static void index() {
+ 		
+ 		List<Ville> villes = (List<Ville>)Cache.get("current"+session.getId());
+ 		if(villes == null){
+ 			villes = Ville.all().fetch();
+ 			Cache.set("current"+session.getId(), villes, "10mn");
+ 		}
+ 		
+ 		render(villes);
+ 	}
 
 }
