d98d50de0b0f35bb4f49c1b6848a30ea5b5313b0
benjamin92.lhuillier@gmail.com <benjamin92.lhuillier@gmail.com@61d47979-7446-dd67-8634-f3ecf0293d68>
2012-12-06 19:12:23 +0000

    Création du Bean Monument

---
+++ Patrimoine FR/app/models/Monument.java
@@ -0,0 +1,37 @@
+package models;
+ 
+import java.util.*;
+import javax.persistence.*;
+ 
+import play.db.jpa.*;
+import play.data.validation.*;
+ 
+@Entity
+public class Monument extends Model {
+ 
+	@Required
+	public String nom_mon;
+	
+	@Lob
+	@MaxSize(10000)
+	public String histo_mon;
+	
+	@Required
+	public float lat_mon;
+	
+	@Required
+	public float long_mon;
+	
+	@ManyToOne
+    @Required
+	public int ville;
+	    
+    public Monument(Ville ville, String nom_mon, String histo_mon, float lat_mon, float long_mon) {
+        this.ville = ville;
+        this.nom_mon = nom_mon;
+        this.histo_mon = histo_mon;
+        this.lat_mon = lat_mon;
+        this.long_mon = long_mon;
+    }
+ 
+}
